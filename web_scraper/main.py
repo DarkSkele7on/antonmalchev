96b223cdb44f13696e3f608258c769969c5e9213
Anton Malchev <tonmalchev@gmail.com>
2023-01-24 13:00:28 +0200

    Create main.py

---
+++ web_scraper/main.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+import sys
+
+class WebScraper:
+    def __init__(self, pages):
+        self.pages = pages
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+        self.proxies = {
+              'http': 'http://IP:PORT',
+              'https': 'http://IP:PORT',
+        }
+    
+    def get_data(self, url):
+        try:
+            response = requests.get(url, headers=self.headers, proxies=self.proxies)
+        except:
+            response = requests.get(url, headers=self.headers)
+        soup = BeautifulSoup(response.content, "html.parser")
+        title = soup.find("title").get_text()
+        return title
+
+    def get_data_from_multiple_pages(self):
+        data = []
+        for page in self.pages:
+            data.append(self.get_data(page))
+        return data
+
+    def write_to_csv(self, data):
+        for url, title in zip(self.pages, data):
+            file_name = url.split(".")[1].replace("/", "") + ".csv"
+            df = pd.DataFrame({'Title': [title]})
+            if not os.path.exists(file_name):
+                df.to_csv(file_name, index=False)
+            else:
+                df.to_csv(file_name, mode='a', header=False, index=False)
+
+if __name__ == "__main__":
+    pages = sys.argv[1:]
+    scraper = WebScraper(pages)
+    data = scraper.get_data_from_multiple_pages()
+    scraper.write_to_csv(data)
